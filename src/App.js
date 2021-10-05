d45c3a7bba9d71f653317bb3fc35000e144762ed
mverkruyse <mverkruyse@comcast.net>
2021-10-05 13:18:58 -0700

    second commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-container">
+        <section data-description="Add emphasis with color">
+          <div class="wrapper">
+            <h1 class="mega montserrat bold">
+            Sell What's <span class="color-emphasis-1">Available Now</span>
+              
+            </h1>
+            <h2 class=" montserrat bold">
+              <span class="color-emphasis-1">SWAN</span> by AES
+            </h2>
+
+          </div>
+        </section>
+
+        
+        
+        <header className="App-header">
+          <div class="container">
+            <div class="responsive-iframe" data-type="AwesomeTableView" data-filters="" data-viewID="-MlGc8DtiFcznQ8WiDQe"></div>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
